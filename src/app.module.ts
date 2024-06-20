9fe5c7f94c58e5c4bbc30f9a8978ccdc0daa1ba0
hobiJeong <jjb26433@naver.com>
2024-06-21 02:18:12 +0900

    feat(#6): LoggerMiddleware 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { ApiModule } from '@src/api/api.module';
 import { BootstrapService } from '@src/bootstrap.service';
 import { CoreModule } from '@src/core/core.module';
+import { LoggerMiddleware } from '@src/middlewares/logger.middleware';
 
 @Module({
   imports: [ApiModule, CoreModule],
   providers: [BootstrapService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}

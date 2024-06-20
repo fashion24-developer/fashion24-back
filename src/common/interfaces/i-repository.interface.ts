2115895694504df27cd578bccb65e45b23cb8833
hobiJeong <jjb26433@naver.com>
2024-06-21 04:14:26 +0900

    feat(#6): service, repository interface 생성

---
+++ src/common/interfaces/i-repository.interface.ts
@@ -0,0 +1,4 @@
+/**
+ * @todo Prisma setup 시에 작성
+ */
+export interface IRepository {}
---
+++ src/common/interfaces/i-service.ts
@@ -0,0 +1,7 @@
+export class IService {
+  create: () => void;
+  findAll: () => void;
+  findOne: () => void;
+  update: () => void;
+  delete: () => void;
+}

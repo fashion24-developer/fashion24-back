fd2d36de64269a65d42a3690f2c8f1f722a84931
NicoDora <pjh76574734@gmail.com>
2024-09-09 15:44:54 +0900

    📬 api(#35): token payload dto에 userRole 필드 옵셔널 설정 삭제

--- src/api/auth/dtos/token-payload.dto.ts
+++ src/api/auth/dtos/token-payload.dto.ts
@@ -3,5 +3,5 @@ import { TokenSubEnum } from '@src/api/auth/enums/token-sub.enum';
 export class TokenPayloadDto {
   sub: TokenSubEnum;
   userId: number;
-  userRole?: string;
+  userRole: string;
 }

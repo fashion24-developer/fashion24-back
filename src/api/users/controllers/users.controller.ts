534f50b4a100adcc00cbd1789e2002719f3adf9c
hobiJeong <jjb26433@naver.com>
2024-09-10 21:10:12 +0900

    feat(#41): error swagger builder 선언 및 apply at test controller

--- src/api/users/controllers/users.controller.ts
+++ src/api/users/controllers/users.controller.ts
@@ -3,38 +3,42 @@ import { ApiTags } from '@nestjs/swagger';
 
 import { ApiUsers } from '@src/api/users/controllers/users.swagger';
 import { UsersService } from '@src/api/users/services/users.service';
+import { globalPrefix } from '@src/bootstrap.service';
+import { routesV1 } from '@src/configs/app.route';
 
 @ApiTags('user')
-@Controller('users')
+@Controller(routesV1.version)
 export class UsersController {
+  static path = `/${globalPrefix}/${routesV1.version}/${routesV1.user.root}`;
+
   constructor(private readonly usersService: UsersService) {}
 
   @ApiUsers.Create({ summary: '유저 생성 API' })
-  @Post()
+  @Post(routesV1.user.root)
   create() {
     throw new MethodNotAllowedException('Method not allowed');
   }
 
   @ApiUsers.FindAll({ summary: '유저 전체 조회 API' })
-  @Get()
+  @Get(routesV1.user.root)
   findAll() {
     throw new MethodNotAllowedException('Method not allowed');
   }
 
   @ApiUsers.FindOne({ summary: '유저 단일 조회 API' })
-  @Get(':id')
+  @Get(routesV1.user.findOne)
   findOne() {
     throw new MethodNotAllowedException('Method not allowed');
   }
 
   @ApiUsers.Update({ summary: '유저 PATCH 수정 API' })
-  @Patch(':id')
+  @Patch(routesV1.user.patch)
   update() {
     throw new MethodNotAllowedException('Method not allowed');
   }
 
   @ApiUsers.Remove({ summary: '유저 삭제 API' })
-  @Delete(':id')
+  @Delete(routesV1.user.delete)
   remove() {
     throw new MethodNotAllowedException('Method not allowed');
   }
--- src/api/users/controllers/users.swagger.ts
+++ src/api/users/controllers/users.swagger.ts
@@ -1,7 +1,12 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiMethodNotAllowedResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiOperation } from '@nestjs/swagger';
 
 import { UsersController } from '@src/api/users/controllers/users.controller';
+import {
+  COMMON_ERROR_HTTP_STATUS_CODE,
+  COMMON_ERROR_HTTP_STATUS_MESSAGE
+} from '@src/common/constants/common.constant';
+import { ErrorSwaggerBuilder } from '@src/common/decorators/error-swagger-builder.decorator';
 import { ApiOperationOptionsWithSummary, ApiOperator } from '@src/common/types/common.type';
 
 export const ApiUsers: ApiOperator<keyof UsersController> = {
@@ -10,7 +15,26 @@ export const ApiUsers: ApiOperator<keyof UsersController> = {
       ApiOperation({
         ...apiOperationOptions
       }),
-      ApiMethodNotAllowedResponse()
+      ErrorSwaggerBuilder(
+        COMMON_ERROR_HTTP_STATUS_CODE.METHOD_NOT_ALLOWED,
+        COMMON_ERROR_HTTP_STATUS_MESSAGE[405],
+        [
+          {
+            description: '허용되지 않은 HTTP 메서드를 사용했을 때.',
+            message: 'Method not allowed'
+          },
+          {
+            description: '허용되지 않은 HTTP 메서드를 어쩌구.',
+            message: 'Method not 어쩌구'
+          },
+          { description: '허용되지 않은 HTTP 메서.', message: ['Method', 'not', 'allowed'] },
+          {
+            description: '허용 어쩌구 HTTP 어쩌구',
+            message: ['Method', 'not', 'allowed', '어쩌구', '저쩌구']
+          }
+        ],
+        UsersController.path
+      )
     );
   },
 
@@ -19,7 +43,14 @@ export const ApiUsers: ApiOperator<keyof UsersController> = {
       ApiOperation({
         ...apiOperationOptions
       }),
-      ApiMethodNotAllowedResponse()
+      ErrorSwaggerBuilder(
+        COMMON_ERROR_HTTP_STATUS_CODE.METHOD_NOT_ALLOWED,
+        COMMON_ERROR_HTTP_STATUS_MESSAGE[405],
+        [
+          { description: '허용되지 않은 HTTP 메서드를 사용했을 때.', message: 'Method not allowed' }
+        ],
+        UsersController.path
+      )
     );
   },
 
@@ -28,7 +59,14 @@ export const ApiUsers: ApiOperator<keyof UsersController> = {
       ApiOperation({
         ...apiOperationOptions
       }),
-      ApiMethodNotAllowedResponse()
+      ErrorSwaggerBuilder(
+        COMMON_ERROR_HTTP_STATUS_CODE.METHOD_NOT_ALLOWED,
+        COMMON_ERROR_HTTP_STATUS_MESSAGE[405],
+        [
+          { description: '허용되지 않은 HTTP 메서드를 사용했을 때.', message: 'Method not allowed' }
+        ],
+        `${UsersController.path}/1`
+      )
     );
   },
 
@@ -37,7 +75,14 @@ export const ApiUsers: ApiOperator<keyof UsersController> = {
       ApiOperation({
         ...apiOperationOptions
       }),
-      ApiMethodNotAllowedResponse()
+      ErrorSwaggerBuilder(
+        COMMON_ERROR_HTTP_STATUS_CODE.METHOD_NOT_ALLOWED,
+        COMMON_ERROR_HTTP_STATUS_MESSAGE[405],
+        [
+          { description: '허용되지 않은 HTTP 메서드를 사용했을 때.', message: 'Method not allowed' }
+        ],
+        `${UsersController.path}/1`
+      )
     );
   },
 
@@ -46,7 +91,14 @@ export const ApiUsers: ApiOperator<keyof UsersController> = {
       ApiOperation({
         ...apiOperationOptions
       }),
-      ApiMethodNotAllowedResponse()
+      ErrorSwaggerBuilder(
+        COMMON_ERROR_HTTP_STATUS_CODE.METHOD_NOT_ALLOWED,
+        COMMON_ERROR_HTTP_STATUS_MESSAGE[405],
+        [
+          { description: '허용되지 않은 HTTP 메서드를 사용했을 때.', message: 'Method not allowed' }
+        ],
+        `${UsersController.path}/1`
+      )
     );
   }
 };

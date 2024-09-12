d16eaee8b4d126b5962c911b2df0c6f879491d7d
JYE <jo1814@naver.com>
2024-09-12 14:14:25 +0900

    ✨ feat(#40): I-fancy-repositroy, findAll과 count 추가

--- src/api/fancy/repositories/i-fancy-repository.interface.ts
+++ src/api/fancy/repositories/i-fancy-repository.interface.ts
@@ -4,4 +4,6 @@ import { IRepository } from '@src/common/interfaces/i-repository.interface';
 
 export interface IFancyRepository extends IRepository {
   create(data: Prisma.FancyCreateInput): Promise<Fancy>;
+  findAll(data: Prisma.FancyFindManyArgs): Promise<Fancy[]>;
+  count(data?: Prisma.FancyCountArgs): Promise<number>;
 }

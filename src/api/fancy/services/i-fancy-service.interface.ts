b609b6b240ec54bfb4047f0c3c20efbba09c34b0
JYE <jo1814@naver.com>
2024-09-12 14:45:55 +0900

    ✏️ modify(#40): I-Fancy-Service, findAll 반환타입 변경

--- src/api/fancy/services/i-fancy-service.interface.ts
+++ src/api/fancy/services/i-fancy-service.interface.ts
@@ -1,12 +1,12 @@
 import { Fancy } from '@prisma/client';
 
 import { CreateFancyDto } from '@src/api/fancy/dtos/create-fancy.dto';
-import { PaginationResponseDto } from '@src/common/dtos/pagination/pagination-response.dto';
 import { IService } from '@src/common/interfaces/i-service.interface';
+import { IPaginationMeta } from '@src/common/interfaces/pagination/i-pagination-meta.interface';
 
 import { FindAllFancyDto } from '../dtos/find-all-fancy.dto';
 
 export interface IFancyService extends IService {
   create(fancyData: CreateFancyDto): Promise<Fancy>;
-  findAll(paginationData: FindAllFancyDto): Promise<PaginationResponseDto<Fancy>>;
+  findAll(paginationData: FindAllFancyDto): Promise<{ data: Fancy[]; meta: IPaginationMeta }>;
 }

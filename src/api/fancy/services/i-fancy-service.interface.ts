265a948e40088890c67ba6b8e837ff0806ed659d
JYE <jo1814@naver.com>
2024-09-12 14:15:22 +0900

    ✨ feat(#40): I-fancy-service, findAll 추가

--- src/api/fancy/services/i-fancy-service.interface.ts
+++ src/api/fancy/services/i-fancy-service.interface.ts
@@ -1,8 +1,12 @@
 import { Fancy } from '@prisma/client';
 
 import { CreateFancyDto } from '@src/api/fancy/dtos/create-fancy.dto';
+import { PaginationResponseDto } from '@src/common/dtos/pagination/pagination-response.dto';
 import { IService } from '@src/common/interfaces/i-service.interface';
+
+import { FindAllFancyDto } from '../dtos/find-all-fancy.dto';
 
 export interface IFancyService extends IService {
   create(fancyData: CreateFancyDto): Promise<Fancy>;
+  findAll(paginationData: FindAllFancyDto): Promise<PaginationResponseDto<Fancy>>;
 }

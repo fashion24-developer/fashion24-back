fe48b8fccb4465cc4ff86dbd06c11592b8c0517a
JYE <jo1814@naver.com>
2024-09-13 15:23:38 +0900

    ♻️ refactor(#40): DI Token, 철자 변경

--- src/common/constants/di.tokens.ts
+++ src/common/constants/di.tokens.ts
@@ -1,7 +1,7 @@
-export const AUTH_SERVICE_DI_TOKEN = 'AuthServiceDiToken';
-export const TOKEN_SERVICE_DI_TOKEN = 'TokenServiceDiToken';
-export const USERS_SERVICE_DI_TOKEN = 'UsersServiceDiToken';
-export const FANCY_SERVICE_DI_TOKEN = 'FancyServiceDiToken';
+export const AUTH_SERVICE_DI_TOKEN = 'AuthServiceDIToken';
+export const TOKEN_SERVICE_DI_TOKEN = 'TokenServiceDIToken';
+export const USERS_SERVICE_DI_TOKEN = 'UsersServiceDIToken';
+export const FANCY_SERVICE_DI_TOKEN = 'FancyServiceDIToken';
 
-export const USERS_REPOSITORY_DI_TOKEN = 'UsersRepositoryDiToken';
-export const FANCY_REPOSITORY_DI_TOKEN = 'FancyRepositoryDiToken';
+export const USERS_REPOSITORY_DI_TOKEN = 'UsersRepositoryDIToken';
+export const FANCY_REPOSITORY_DI_TOKEN = 'FancyRepositoryDIToken';

d5e962b58187aaf51fe814ed3474453243f8c980
JYE <jo1814@naver.com>
2024-09-19 15:14:16 +0900

    ✨ feat(#40): fancy를 포함한 관련된 Entity 생성

---
+++ src/api/sub-options/entity/sub-option.entity.ts
@@ -0,0 +1,28 @@
+import { FancySubOptionEntity } from '@src/api/fancy-sub-options/entity/fancy-sub-option.entity';
+import { OptionEntity } from '@src/api/options/entity/option.entity';
+
+export class SubOptionEntity {
+  /**
+   * Primary Key
+   */
+  private readonly _id: number;
+  /**
+   * 옵션 아이디 (FK)
+   */
+  private _optionId: number;
+  /**
+   * 소옵션 이름
+   */
+  private _name: string;
+  /**
+   * 소옵션 추가 금액
+   */
+  private _additionalPrice: number;
+  /**
+   * 생성일
+   */
+  private _createdAt: Date;
+
+  private _fancySubOptions: FancySubOptionEntity[];
+  private _option: OptionEntity;
+}

a9f64d31c7b92df91ca9b1ecbd5605f7fea909ed
JYE <jo1814@naver.com>
2024-09-19 18:21:18 +0900

    ✨ feat(#40): Fancy Controller 관련 경로  재설정, PaginationDTO이용한 타입을 지정한 반환 등

--- src/api/fancy/controllers/fancy.controller.ts
+++ src/api/fancy/controllers/fancy.controller.ts
@@ -1,22 +1,32 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-import { ApiFancy } from '@src/api/fancy/controllers/fancy.swagger';
+import { ApiFancyDefault } from '@src/api/fancy/controllers/fancy-default.swagger';
+import { FindAllFancyResponseDto } from '@src/api/fancy/dtos/find-all-fancy-response.dto';
 import { FindAllFancyDto } from '@src/api/fancy/dtos/find-all-fancy.dto';
-import { IFancyService } from '@src/api/fancy/services/i-fancy-service.interface';
-import { FANCY_SERVICE_DI_TOKEN } from '@src/common/constants/di.tokens';
+import { FancyService } from '@src/api/fancy/services/fancy.service';
+import { globalPrefix } from '@src/bootstrap.service';
 import { PaginationResponseDto } from '@src/common/dtos/pagination/pagination-response.dto';
+import { routesV1 } from '@src/configs/app.route';
 
+@UseGuards()
 @ApiTags('fancy')
-@Controller('fancy')
+@Controller(routesV1.version)
 export class FancyController {
-  constructor(@Inject(FANCY_SERVICE_DI_TOKEN) private readonly fancyService: IFancyService) {}
+  static path = `/${globalPrefix}/${routesV1.version}/${routesV1.fancy.root}`;
 
-  @ApiFancy.FindAll({ summary: 'fancy 전체 조회' })
-  @Get()
-  async findAll(@Query() paginationData: FindAllFancyDto) {
-    const { data, meta } = await this.fancyService.findAll(paginationData);
+  constructor(private readonly fancyService: FancyService) {}
 
-    return new PaginationResponseDto(data, meta);
+  @ApiFancyDefault.FindAllForPagination({ summary: 'fancy 전체 조회' })
+  @Get(routesV1.fancy.findAllForPagination)
+  async findAllForPagination(@Query() paginationData: FindAllFancyDto) {
+    const { data, meta } = await this.fancyService.findAllForPagination(paginationData);
+
+    return new PaginationResponseDto(
+      data.map((data) => {
+        return new FindAllFancyResponseDto(data);
+      }),
+      meta
+    );
   }
 }

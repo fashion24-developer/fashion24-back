f98ff529305b52f8b90a9cc42689af8b96f449bf
JYE <jo1814@naver.com>
2024-09-19 16:01:59 +0900

    ✏️ modify(#40): FindAllFancyDTO 프로피터 명 변경 및 validation 추가

--- src/api/fancy/dtos/find-all-fancy.dto.ts
+++ src/api/fancy/dtos/find-all-fancy.dto.ts
@@ -1,50 +1,54 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
-import { IsEnum, IsInt, IsOptional, IsString } from 'class-validator';
+import { ArrayMaxSize, IsEnum, IsInt, IsOptional, IsString, MaxLength } from 'class-validator';
 
 import { TransformValueToNumberArray } from '@src/api/fancy/decorators/value-to-number-array.decorator';
-import { FancyOrderBy, FancyOrderByType } from '@src/api/fancy/enums/fancy-orderby.enum';
-import {
-  FancyProductStatus,
-  FancyProductStatusType
-} from '@src/api/fancy/enums/fancy-product-status.enum';
+import { FancyOrderBy } from '@src/api/fancy/enums/fancy-orderby.enum';
+import '@src/api/fancy/enums/fancy-product-status.enum';
+import { FancyProductStatus } from '@src/api/fancy/enums/fancy-product-status.enum';
 import { PaginationDto } from '@src/common/dtos/pagination/pagination.dto';
-import { SortOption, SortOptionType } from '@src/common/enums/sort-option.enum';
+import { SortOption } from '@src/common/enums/sort-option.enum';
+import { ValueOf } from '@src/common/types/common.type';
 
 export class FindAllFancyDto extends PaginationDto {
-  @ApiPropertyOptional({ description: '완제품 이름' })
+  @ApiPropertyOptional({ description: '완제품 이름', maxLength: 50 })
   @IsString()
+  @MaxLength(50)
   @IsOptional()
   name?: string;
 
   @ApiPropertyOptional({ description: '상품 상태', enum: FancyProductStatus })
   @IsEnum(FancyProductStatus)
   @IsOptional()
-  status?: FancyProductStatusType;
+  status?: ValueOf<typeof FancyProductStatus>;
 
-  @ApiPropertyOptional({ description: '옵션 ID' })
+  @ApiPropertyOptional({ description: '옵션 ID', maxItems: 5, type: [Number] })
   @TransformValueToNumberArray()
   @IsInt({ each: true })
+  @ArrayMaxSize(5)
   @IsOptional()
-  optionName?: number[];
+  optionId?: number[];
 
-  @ApiPropertyOptional({ description: '소옵션 ID' })
+  @ApiPropertyOptional({ description: '소옵션 ID', maxItems: 5, type: [Number] })
   @TransformValueToNumberArray()
   @IsInt({ each: true })
+  @ArrayMaxSize(5)
   @IsOptional()
-  subOptionName?: number[];
+  subOptionId?: number[];
 
-  @ApiPropertyOptional({ description: '룩 ID' })
+  @ApiPropertyOptional({ description: '룩 ID', maxItems: 5, type: [Number] })
   @TransformValueToNumberArray()
   @IsInt({ each: true })
+  @ArrayMaxSize(5)
   @IsOptional()
-  lookName?: number[];
+  lookId?: number[];
 
-  @ApiPropertyOptional({ description: '태그 ID' })
+  @ApiPropertyOptional({ description: '태그 ID', maxItems: 5, type: [Number] })
   @TransformValueToNumberArray()
   @IsInt({ each: true })
+  @ArrayMaxSize(5)
   @IsOptional()
-  tagName?: number[];
+  tagId?: number[];
 
   @ApiPropertyOptional({
     description: '정렬 기준',
@@ -52,7 +56,7 @@ export class FindAllFancyDto extends PaginationDto {
     default: FancyOrderBy.PRICE
   })
   @IsEnum(FancyOrderBy)
-  orderBy: FancyOrderByType = FancyOrderBy.PRICE;
+  orderBy: ValueOf<typeof FancyOrderBy> = FancyOrderBy.PRICE;
 
   @ApiPropertyOptional({
     description: '정렬 방향',
@@ -60,5 +64,5 @@ export class FindAllFancyDto extends PaginationDto {
     default: SortOption.DESC
   })
   @IsEnum(SortOption)
-  orderDirection: SortOptionType = SortOption.DESC;
+  orderDirection: ValueOf<typeof SortOption> = SortOption.DESC;
 }

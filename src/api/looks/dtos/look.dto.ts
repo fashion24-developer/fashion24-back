b65bf056b4602e0d8bf6768bd95c6e9108352285
JYE <jo1814@naver.com>
2024-09-20 17:48:11 +0900

    ✨ feat(#61): LookDto ApiProperty 추가

--- src/api/looks/dtos/look.dto.ts
+++ src/api/looks/dtos/look.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LookDto {
+  @ApiProperty({ description: 'ID' })
   id: number;
+
+  @ApiProperty({ description: 'Look 이름' })
   name: string;
+
+  @ApiProperty({ description: 'Look 이미지 URL' })
   imageUrl: string;
+
+  @ApiProperty({ description: '생성일' })
   createdAt: Date;
 }

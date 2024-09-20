626fec07707f78c142dcbf8ca02464be58056881
JYE <jo1814@naver.com>
2024-09-20 14:49:28 +0900

    ✨ feat(#61): apiModule에 LookMoule 추가

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '@src/api/auth/auth.module';
 import { FancyModule } from '@src/api/fancy/fancy.module';
+import { LookModule } from '@src/api/looks/look.module';
 import { UsersModule } from '@src/api/users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, FancyModule]
+  imports: [AuthModule, UsersModule, FancyModule, LookModule]
 })
 export class ApiModule {}

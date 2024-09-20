6aab3281a4eaec7dcd71ef8e177a3e7f648eceee
JYE <jo1814@naver.com>
2024-09-20 18:04:50 +0900

    ✨ feat(#61): Look 핵심 로직 구현

--- src/api/looks/controllers/look.controller.ts
+++ src/api/looks/controllers/look.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
+import { AccessTokenAuthGuard } from '@src/api/auth/jwt/jwt-auth.guard';
+import { ApiLook } from '@src/api/looks/controllers/look.swagger';
+import { LookResponseDto } from '@src/api/looks/dtos/look-response.dto';
+import { LookService } from '@src/api/looks/services/look.service';
 import { globalPrefix } from '@src/bootstrap.service';
 import { routesV1 } from '@src/configs/app.route';
 
-@ApiTags('Look')
-@Controller('look')
+@UseGuards(AccessTokenAuthGuard)
+@ApiTags('look')
+@Controller(routesV1.version)
 export class LookController {
   static path = `${globalPrefix}` + `${routesV1.version}` + `${routesV1.look.root}`;
 
-  constructor() {}
+  constructor(private readonly lookService: LookService) {}
 
+  @ApiLook.FindAll({ summary: '룩 전체 조회' })
   @Get(routesV1.look.findAll)
-  findAll() {
-    return;
+  async findAll(): Promise<LookResponseDto[]> {
+    const look = await this.lookService.findAll();
+
+    return look.map((item) => new LookResponseDto(item));
   }
 }

7ef6cd92d81d2d90946124928a067aa3c511551b
JYE <jo1814@naver.com>
2024-09-20 13:27:56 +0900

    ✨ feat(#61): LOOK_REPOSITORY_DI_TOKEN 생성

--- src/common/constants/di.tokens.ts
+++ src/common/constants/di.tokens.ts
@@ -4,3 +4,4 @@ export const USERS_SERVICE_DI_TOKEN = 'UsersServiceDIToken';
 
 export const USERS_REPOSITORY_DI_TOKEN = 'UsersRepositoryDIToken';
 export const FANCY_REPOSITORY_DI_TOKEN = 'FancyRepositoryDIToken';
+export const LOOK_REPOSITORY_DI_TOKEN = 'LookRepositoryDIToken';

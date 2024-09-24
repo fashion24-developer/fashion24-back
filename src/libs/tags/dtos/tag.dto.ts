b422b6d25791e02226c6c457f135af369417fc1c
JYE <jo1814@naver.com>
2024-09-24 16:36:19 +0900

    ✨ feat(#67): fancy-image, tag DTO ApiProperty 추가

--- src/libs/tags/dtos/tag.dto.ts
+++ src/libs/tags/dtos/tag.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class TagDto {
+  @ApiProperty({ description: 'ID', example: 1 })
   id: number;
+
+  @ApiProperty({ description: '태그 이름', example: '여름' })
   name: string;
+
+  @ApiProperty({ description: '생성일', example: '2024-09-05T12:15:54.194Z' })
   createdAt: Date;
 }

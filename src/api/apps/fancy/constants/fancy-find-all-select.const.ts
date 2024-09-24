e5f54bf7b5f26714d9413d010ec04baee489fa64
JYE <jo1814@naver.com>
2024-09-24 16:23:12 +0900

    🔨 refactor(#67):  FANCY_FIND_ALL_SELECT 관련 select 문 수정

--- src/api/apps/fancy/constants/fancy-find-all-select.const.ts
+++ src/api/apps/fancy/constants/fancy-find-all-select.const.ts
@@ -6,10 +6,16 @@ export const FANCY_FIND_ALL_SELECT = {
   discountRate: true,
   status: true,
   fancyImages: { select: { id: true, imageUrl: true }, where: { order: 1 } },
-  fancyOptions: { select: { option: { select: { id: true, name: true } } } },
+  fancyOptions: {
+    select: { option: { select: { id: true, name: true } } },
+    orderBy: { optionId: 'asc' }
+  },
   fancySubOptions: {
-    select: { subOption: { select: { id: true, name: true, additionalPrice: true } } }
+    select: {
+      subOption: { select: { id: true, optionId: true, name: true, additionalPrice: true } }
+    },
+    orderBy: { subOptionId: 'asc' }
   },
   looks: { select: { id: true, name: true, imageUrl: true } },
-  tags: { select: { id: true, name: true } }
+  tags: { select: { id: true, name: true }, orderBy: { id: 'asc' } }
 } as const;

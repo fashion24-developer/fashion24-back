ac83220a458613078cb5f207c98dbe00e707b5e2
JYE <jo1814@naver.com>
2024-09-24 16:28:31 +0900

    🔨 refactor(#67):  PaginationResponseDto, ApiPrpoerty관련 속성추가

--- src/common/dtos/pagination/pagination-response.dto.ts
+++ src/common/dtos/pagination/pagination-response.dto.ts
@@ -1,10 +1,17 @@
-import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
+import { ApiExtraModels, ApiProperty, getSchemaPath } from '@nestjs/swagger';
 
+import { FancyPaginationResponseDto } from '@src/api/apps/fancy/dtos/find-all-fancy-response.dto';
 import { PaginationMetaDto } from '@src/common/dtos/pagination/pagination-meta.dto';
 
 @ApiExtraModels()
 export class PaginationResponseDto<T> {
-  @ApiProperty({ type: 'array' })
+  // T에 들어갈 수 있는 DTO같은것들 oneOf 배열안에 묶어서 넣어주면 문서화 됩니다.
+  @ApiProperty({
+    type: 'array',
+    items: {
+      oneOf: [{ $ref: getSchemaPath(FancyPaginationResponseDto) }]
+    }
+  })
   data: Array<T>;
 
   @ApiProperty({ type: PaginationMetaDto })
